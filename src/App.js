c46e96d1acea22bf855501ac2ea1f85ac035e4c1
Nhat Tran <nhattran@Nhats-MacBook-Air.local>
2023-11-11 21:56:29 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Import the CSS file
 
-function App() {
+
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [recipes, setRecipes] = useState([]);
+
+  const API_ENDPOINT = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+  const API_RANDOM_ENDPOINT = `https://www.themealdb.com/api/json/v1/1/random.php`;
+
+  const getRecipes = () => {
+    fetch(API_ENDPOINT)
+      .then(response => response.json())
+      .then(data => {
+        setRecipes(data.meals);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  };
+
+  const getRandomRecipe = () => {
+    fetch(API_RANDOM_ENDPOINT)
+      .then(response => response.json())
+      .then(data => {
+        setRecipes([data.meals[0]]);
+      })
+      .catch(error => {
+        console.error('Error fetching random recipe:', error);
+      });
+  };
+
+  const handleChange = e => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    getRecipes();
+    setQuery('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title">Find Recipe</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={query} onChange={handleChange} placeholder="Please type your recipe" />
+        <button type="submit">Search</button>
+      </form>
+      <button onClick={getRandomRecipe}>Random Recipe</button>
+      <div>
+        {recipes &&
+          recipes.map(recipe => (
+            <div className="recipe-card" key={recipe.idMeal}>
+              <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+              <h3>{recipe.strMeal}</h3>
+              <p>{recipe.strInstructions}</p>
+            </div>
+          ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
